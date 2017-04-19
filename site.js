83f782b7604a010d6dc38c77ec260fd0cf20c722
ardalan@mobilefringe.com @ CodeCloud <ardalan@mobilefringe.com @ CodeCloud>
2017-04-19 17:00:17 -0400

    Plaza Paseo Real 1.2  /site.js

--- site.js
+++ site.js
@@ -1,6 +1,6 @@
 var site_json = {
-                    "name" : "The District at Tustin Legacy",
+                    "name" : "Plaza Paseo Real",
                     "default_image" : "//codecloud.cdn.speedyrails.net/sites/58f648356e6f645736040000/image/jpeg/1492483702000/ppr_500x500_logo.jpgg",
                     "time_zone" : "T08:00:00Z",
                     "social_feed" : "//thedistrict.mallmaverick.com/api/v2/district/social.json"
-                }http://assets.codecloudapp.com/sites/58f648356e6f645736040000/image/jpeg/1492483702000/ppr_500x500_logo.jpg
+                }
