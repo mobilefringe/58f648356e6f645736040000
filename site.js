c3e98c125ee222c4994332c54d4e97415a9e73ef
ardalan@mobilefringe.com @ CodeCloud <ardalan@mobilefringe.com @ CodeCloud>
2017-05-11 11:38:08 -0400

    Plaza Paseo Real 1.2  /site.js

--- site.js
+++ site.js
@@ -2,5 +2,5 @@ var site_json = {
                     "name" : "Plaza Paseo Real",
                     "default_image" : "//codecloud.cdn.speedyrails.net/sites/58f648356e6f645736040000/image/jpeg/1492483702000/ppr_500x500_logo.jpgg",
                     "time_zone" : "T08:00:00Z",
-                    "social_feed" : "//thedistrict.mallmaverick.com/api/v2/district/social.json"
+                    "social_feed" : "//paseoreal.mallmaverick.com/api/v3/paseoreal/social.json"
                 }
