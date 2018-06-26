609fbbbf435a7cc7f2e05d3ca46bf77b02ed892f
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2018-06-26 11:06:46 -0400

    Plaza Paseo Real 1.4  /site.js

---
+++ site.js
@@ -0,0 +1,6 @@
+var site_json = {
+                    "name" : "Plaza Paseo Real",
+                    "default_image" : "//codecloud.cdn.speedyrails.net/sites/58f648356e6f645736040000/image/jpeg/1492483702000/ppr_500x500_logo.jpg",
+                    "time_zone" : "T08:00:00Z",
+                    "social_feed" : "//paseoreal.mallmaverick.com/api/v3/paseoreal/social.json"
+                }
